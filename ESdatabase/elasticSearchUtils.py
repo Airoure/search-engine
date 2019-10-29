86139cf677b7e0d131d3393031a8e6729f7afb9f
Phalaty <48918112+Phalaty@users.noreply.github.com>
2019-10-29 22:43:01 +0800

    代码命名规范化，添加部分函数
    环境需求：ElasticSearch 7.x；python的elasticsearch库；

--- ESdatabase/elasticSearchUtils.py
+++ ESdatabase/elasticSearchUtils.py
@@ -3,19 +3,20 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 
-def addIndex(index):
+def add_index(index):
     result = es.indices.create(index=index, ignore=400)
     for i in result:
         print(i)
 
 
-def deleteIndex(index):
+def delete_index(index):
     result = es.indices.delete(index=index, ignore=[400, 404])
     for i in result:
         print(i)
 
 
-def addToElastic(index=None, title=None, content=None, url=None, _type=None, date=None):
+def add_to_elastic(index=None, title=None, content=None, url=None, _type=None,
+                   date=None):
     data = {
         'title': title,
         'content': content,
@@ -24,5 +25,23 @@ def addToElastic(index=None, title=None, content=None, url=None, _type=None, dat
         'date': date
     }
     es.index(index=index, doc_type=_type, body=data)
+
+
+def add_index_mapping(index=None, doc_type=None, field=None,
+                      mapping_type='text', analyzer='ik_max_word',
+                      search_analyzer='ik_smart'):
+    mapping = {
+        'properties': {
+            field: {
+                'type': mapping_type,
+                'analyzer': analyzer,
+                'search_analyzer': search_analyzer
+            }
+        }
+    }
+    result = es.indices.put_mapping(index=index, doc_type=doc_type,
+                                    body=mapping, include_type_name=True)
+    for i in result:
+        print(i)
 
 
