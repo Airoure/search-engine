559782e52ea31717373aa987ab1f3de34bb9a1e0
Phalaty <48918112+Phalaty@users.noreply.github.com>
2019-10-30 22:40:54 +0800

    添加条目的封装类，添加函数的注释，修改函数名以符合规范
    环境需求：ElasticSearch 7.x；elasticsearch-ik 7.x；python elasticsearch第三方库

---
+++ ESdatabase/elasticSearchUtils.py
@@ -0,0 +1,182 @@
+import json
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch()
+
+IK_MAX_WORD = 'ik_max_word'
+IK_SMART = 'ik_smart'
+
+
+def add_index(index):
+    '''
+    添加索引
+    :param index: 要添加的索引名称
+    :return: 无返回值
+    '''
+    result = es.indices.create(index=index, ignore=400)
+    for i in result:
+        print(i)
+
+
+def delete_index(index):
+    '''
+    删除索引
+    :param index: 要删除的索引名称
+    :return:
+    '''
+    result = es.indices.delete(index=index, ignore=[400, 404])
+    for i in result:
+        print(i)
+
+
+def add_to_elastic(index=None, title=None, content=None, href=None, _type=None,
+                   date=None):
+    '''
+    将一个条目加入到数据库中。
+    :param index: 用于该条目的索引
+    :param title: 该条目的标题
+    :param content: 该条目的内容，方便分词检索
+    :param href: 该条目的地址
+    :param _type: 该条目的类型，检索条目时使用
+    :param date: 该条目的日期，最好是发布日期
+    :return: 在ElasticSearch库中存放的形式，字典
+    '''
+    data = {
+        'title': title,
+        'content': content,
+        'href': href,
+        'type': _type,
+        'date': date
+    }
+    es.index(index=index, doc_type=_type, body=data)
+    return data
+
+
+def add_to_elastic(index=None, record=None):
+    '''
+    另一种加入方法，将构建的Record类对象传入，会自动存入ElasticSearch中
+    :param index: 将使用的索引值
+    :param record: 包含条目所有属性的类对象
+    :return: 一个包含条目所有属性的字典
+    '''
+    data = record.to_dict()
+    es.index(index=index, doc_type=record.get_type(), body=data)
+    return data
+
+
+def add_index_mapping(index=None, doc_type=None, field=['title', 'content'],
+                      mapping_type='text', analyzer='ik_max_word',
+                      search_analyzer='ik_smart'):
+    '''
+    给索引更新mapping，方便分词查询
+    :param index: 要更新mapping的索引名
+    :param doc_type: 文档类型
+    :param field: [],指定需要分词的字段
+    :param mapping_type: 'text'\'keyword'\'number'\'array'\'date'等，
+        指定数据类型，这里默认为'text'类型
+    :param analyzer: 指定使用的分词器，由于默认分词器汉字支持不好，使用ik分词器ik_max_word（极致细分）
+    :param search_analyzer: 搜索时使用的分词器，这里默认为ik_smart（粒度较大）
+    :return:
+    '''
+    mapping = {
+        'properties': {
+            field[0]: {
+                'type': mapping_type,
+                'analyzer': analyzer,
+                'search_analyzer': search_analyzer
+            },
+            field[1]: {
+                'type': mapping_type,
+                'analyzer': analyzer,
+                'search_analyzer': search_analyzer
+            }
+        }
+    }
+    result = es.indices.put_mapping(index=index, doc_type=doc_type,
+                                    body=mapping, include_type_name=True)
+    for i in result:
+        print(i)
+
+
+def search_from_elastic(index=None, dsl=None):
+    '''
+    查询数据库中数据
+    :param index: 索引名称
+    :param dsl: 编写好的查询语句
+    :return: 查询结果的列表，每条查询结果封装成类。
+    '''
+    result = es.search(index=index, body=dsl)
+    print(json.dumps(result, indent=4, ensure_ascii=False))
+    li = []
+    if result['hits']['total']['value'] is not 0:
+        for i in result['hits']['hits']:
+            re = Record(i['_source']['type'],
+                        i['_source']['title'],
+                        i['_source']['href'],
+                        i['_source']['date'],
+                        i['_source']['content'])
+            li.append(re)
+    return li
+
+
+def search_from_elastic(index=None, q_dsl=None):
+    pass
+
+
+class Record:
+    '''
+    将条目封装成一个类，以便直接使用
+    '''
+    def __init__(self, _type, title, href, date, content, id=None):
+        self.type = _type
+        self.title = title
+        self.href = href
+        self.content = content
+        self.date = date
+        self.id = id
+
+    def display(self):
+        print("类型："+self.type)
+        print("标题："+self.title)
+        print("链接："+self.href)
+        print("时间：" + self.date)
+        print("内容：" + self.content)
+
+    def get_type(self):
+        return self.type
+
+    def get_href(self):
+        return self.href
+
+    def get_title(self):
+        return self.title
+
+    def get_content(self):
+        return self.content
+
+    def get_time(self):
+        return self.date
+
+    def get_id(self):
+        return self.id
+
+    def to_dict(self):
+        _dict = {
+            'title': self.title,
+            'content': self.content,
+            'href': self.href,
+            'type': self.type,
+            'date': self.date
+        }
+        return _dict
+
+
+class Dsl:
+    def __init__(self, query=None, delete=None):
+        self.query = query
+        self.delete = delete
+
+    def set_query(self, query):
+        self.query = query
+
+
