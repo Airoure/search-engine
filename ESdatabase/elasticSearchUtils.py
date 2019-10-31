251731c93a904b253e0c624b9a1ef673aaa91202
Phalaty <48918112+Phalaty@users.noreply.github.com>
2019-10-31 21:38:09 +0800

    完善搜索函数（现在可以输出数据库中的所有结果），添加默认搜索函数（只要提供索引与关键词即可）
    环境需求：ElasticSearch 7.x；elasticsearch-analyzer-ik 7.x；python elasticsearch库

--- ESdatabase/elasticSearchUtils.py
+++ ESdatabase/elasticSearchUtils.py
@@ -8,22 +8,22 @@ IK_SMART = 'ik_smart'
 
 
 def add_index(index):
-    '''
+    """
     添加索引
     :param index: 要添加的索引名称
     :return: 无返回值
-    '''
+    """
     result = es.indices.create(index=index, ignore=400)
     for i in result:
         print(i)
 
 
 def delete_index(index):
-    '''
+    """
     删除索引
     :param index: 要删除的索引名称
     :return:
-    '''
+    """
     result = es.indices.delete(index=index, ignore=[400, 404])
     for i in result:
         print(i)
@@ -53,12 +53,12 @@ def add_to_elastic(index=None, title=None, content=None, href=None, _type=None,
 
 
 def add_to_elastic(index=None, record=None):
-    '''
+    """
     另一种加入方法，将构建的Record类对象传入，会自动存入ElasticSearch中
     :param index: 将使用的索引值
     :param record: 包含条目所有属性的类对象
     :return: 一个包含条目所有属性的字典
-    '''
+    """
     data = record.to_dict()
     es.index(index=index, doc_type=record.get_type(), body=data)
     return data
@@ -66,8 +66,8 @@ def add_to_elastic(index=None, record=None):
 
 def add_index_mapping(index=None, doc_type=None, field=['title', 'content'],
                       mapping_type='text', analyzer='ik_max_word',
-                      search_analyzer='ik_smart'):
-    '''
+                      search_analyzer='ik_max_word'):
+    """
     给索引更新mapping，方便分词查询
     :param index: 要更新mapping的索引名
     :param doc_type: 文档类型
@@ -75,65 +75,97 @@ def add_index_mapping(index=None, doc_type=None, field=['title', 'content'],
     :param mapping_type: 'text'\'keyword'\'number'\'array'\'date'等，
         指定数据类型，这里默认为'text'类型
     :param analyzer: 指定使用的分词器，由于默认分词器汉字支持不好，使用ik分词器ik_max_word（极致细分）
-    :param search_analyzer: 搜索时使用的分词器，这里默认为ik_smart（粒度较大）
+    :param search_analyzer: 搜索时使用的分词器，这里默认为ik_max_word（粒度较大）
     :return:
-    '''
-    mapping = {
-        'properties': {
-            field[0]: {
-                'type': mapping_type,
-                'analyzer': analyzer,
-                'search_analyzer': search_analyzer
-            },
-            field[1]: {
-                'type': mapping_type,
-                'analyzer': analyzer,
-                'search_analyzer': search_analyzer
+    """
+    mappings = {
+            'properties': {
+                field[0]: {
+                    'type': mapping_type,
+                    'analyzer': analyzer,
+                    'search_analyzer': search_analyzer
+                },
+                field[1]: {
+                    'type': mapping_type,
+                    'analyzer': analyzer,
+                    'search_analyzer': search_analyzer
+                }
             }
-        }
     }
     result = es.indices.put_mapping(index=index, doc_type=doc_type,
-                                    body=mapping, include_type_name=True)
+                                    body=mappings, include_type_name=True)
     for i in result:
         print(i)
 
 
 def search_from_elastic(index=None, dsl=None):
-    '''
+    """
     查询数据库中数据
     :param index: 索引名称
     :param dsl: 编写好的查询语句
     :return: 查询结果的列表，每条查询结果封装成类。
-    '''
-    result = es.search(index=index, body=dsl)
+    """
+    result = es.search(index=index, body=dsl, scroll='5m', size=1000)
     print(json.dumps(result, indent=4, ensure_ascii=False))
     li = []
-    if result['hits']['total']['value'] is not 0:
-        for i in result['hits']['hits']:
-            re = Record(i['_source']['type'],
-                        i['_source']['title'],
-                        i['_source']['href'],
-                        i['_source']['date'],
-                        i['_source']['content'])
-            li.append(re)
+    total = result['hits']['total']['value']
+    scroll_id = result['_scroll_id']
+    if total is not 0:
+        for k in range(0, int(total/1000)+1):
+            if k is not 0:
+                result = es.scroll(scroll_id=scroll_id, scroll='5m')
+                scroll_id = result['_scroll_id']
+            for i in result['hits']['hits']:
+                re = Record(i['_source']['type'],
+                            i['_source']['title'],
+                            i['_source']['href'],
+                            i['_source']['date'],
+                            i['_source']['content'],
+                            i['_source']['id'])
+                li.append(re)
     return li
 
 
-def search_from_elastic(index=None, q_dsl=None):
-    pass
+def default_search(index=None, keyword=None):
+    """
+    默认搜索函数，默认搜索title、content字段内容。
+    :param index: 索引名
+    :param keyword: 关键字
+    :return: Record对象的列表
+    """
+    dsl = {
+        'query': {
+            'bool': {
+                'should': [
+                    {
+                        'query_string': {
+                            'title': keyword
+                        }
+                    },
+                    {
+                        'query_string': {
+                            'content': keyword
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    li = search_from_elastic(index, dsl)
+    return li
 
 
 class Record:
-    '''
+    """
     将条目封装成一个类，以便直接使用
-    '''
-    def __init__(self, _type, title, href, date, content, id=None):
+    """
+    def __init__(self, _type, title, href, date, content, _id=None):
         self.type = _type
         self.title = title
         self.href = href
         self.content = content
         self.date = date
-        self.id = id
+        self.id = _id
 
     def display(self):
         print("类型："+self.type)
